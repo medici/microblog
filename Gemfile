9197cd272e060394cf8a1aa551e3ef29e4cfd7df
medici <the.medici.florentine@gmail.com>
2013-08-17 12:02:51 +0800

    Initial commit

--- Gemfile
+++ Gemfile
@@ -1,12 +1,19 @@
 source 'https://rubygems.org'
 ruby '1.9.3'
 #ruby-gemset=microblog
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
 	gem 'pg', '0.15.1'
+    gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+    gem 'selenium-webdriver', '2.0.0'
+    gem 'capybara', '2.1.0'
 end
 
 # Use SCSS for stylesheets
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,22 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Microblog::Application.config.secret_key_base = '826670f6fb47191fc6359e55d3165801cb2fcb2d63790dcaf65e7cb00bc68f831baec1ea2b921beebb980bee69652d0e31dd488b3313610304861f76bef84a52'
+
+#Microblog::Application.config.secret_key_base = '826670f6fb47191fc6359e55d3165801cb2fcb2d63790dcaf65e7cb00bc68f831baec1ea2b921beebb980bee69652d0e31dd488b3313610304861f76bef84a52'
+
+require 'securerandom'
+
+def secure_token
+    token_file = Rails.root.join('.secret')
+    if File.exist?(token_file)
+        # Use the existing token.
+        File.read(token_file).chomp
+    else
+        # Generate a new token and store it in token_file.
+        token = SecureRandom.hex(64)
+        File.write(token_file, token)
+        token
+    end
+end
+
+Microblog::Application.config.secret_key_base = secure_token
